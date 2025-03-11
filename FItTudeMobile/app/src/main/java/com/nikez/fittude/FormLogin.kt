a98cb07bc82ad00a1b091c218eb2b8a1b89839a0
JoaoNike <jaogabrie00@gmail.com>
2025-03-11 17:17:02 -0300

    Projeto mobile

---
+++ FItTudeMobile/app/src/main/java/com/nikez/fittude/FormLogin.kt
@@ -0,0 +1,21 @@
+package com.nikez.fittudemobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nikez.fittudemobile.R
+
+class FormLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_form_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.FormLogin)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
