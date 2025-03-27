6c7e833fe367691e44ca37af5767a689b70b832c
Luiz Carlos Souza <150024147+lcsouza2@users.noreply.github.com>
2025-03-27 14:17:50 -0300

    Consertando o problema de case-sensitive

--- API/main.py
+++
@@ -1,8 +0,0 @@
-from data_api.data_delete_routes import DATA_API
-from fastapi import FastAPI
-from routes.user_post_routes import USER_API
-
-MAIN_APP = FastAPI(debug=True)
-
-MAIN_APP.mount("/data", DATA_API)
-MAIN_APP.mount("/user", USER_API)
