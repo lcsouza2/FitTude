0631a1b0af629e6e739150151aa7ce3f6afa0230
jaoNike <jaogabrie00@gmail.com>
2025-05-26 21:15:53 -0300

    Configurando rotas

--- api/main.py
+++ api/main.py
@@ -23,3 +23,23 @@ async def return_register_page(request: Request):
 @MAIN_APP.get("/dashboard")
 async def return_dashboard_page(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
+
+@MAIN_APP.get("/active")
+async def return_active_page(request: Request):
+    return templates.TemplateResponse("active.html", {"request": request})
+
+@MAIN_APP.get("/aparelhos")
+async def return_aparelhos_page(request: Request):
+    return templates.TemplateResponse("aparelhos.html", {"request": request})
+
+@MAIN_APP.get("/landingpage")
+async def return_landing_page(request: Request):
+    return templates.TemplateResponse("landingpage.html", {"request": request})
+
+@MAIN_APP.get("/relatorios")
+async def return_relatorios_page(request: Request):
+    return templates.TemplateResponse("relatorios.html", {"request": request})
+
+@MAIN_APP.get("/configuracoes")
+async def return_configuracoes_page(request: Request):
+    return templates.TemplateResponse("configuracoes.html", {"request": request})
