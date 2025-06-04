f6e1355dea1126a22e8f361d6de6fcc082e5d31f
JoaoNike <jaogabrie00@gmail.com>
2025-06-03 23:20:30 -0300

    fix: Refatorando tratamento de erros na requisição da API

--- web/static/js/core/auth.js
+++ web/static/js/core/auth.js
@@ -4,38 +4,26 @@ export class ApiClient {
     this.baseURL = baseURL;
     }
   async request(endpoint, options = {}) {
-      const response = await fetch(this.baseURL + endpoint, options).then
+    try {
+      const response = await fetch(this.baseURL + endpoint, options);
+      
+      if (!response.ok) {
+        const errorText = await response.text(); // lê a resposta mesmo em erro
+        throw new Error(`Erro ${response.status}: ${errorText}`);
+      }
 
-      if (response.ok) {
-          console.log("200 OK:", response);
-      }
-      else if(response.status == 409) {
-          exibirMensagem('Email já cadastrado.', 'danger');
-          throw new Error(`Erro ${response.status}: ${errorText}`);
-      }
-      else if (response.status == 401) {
-          exibirMensagem('Email ou senha inválidos.', 'danger');
-          throw new Error(`Erro ${response.status}: ${errorText}`);
-
-      }else if (response.status == 404) {
-          exibirMensagem('Falha na comunição com o servido', 'danger');
-          throw new Error(`Erro ${response.status}: ${errorText}`);
-      }   
-      else if (response.status == 500) {
-          exibirMensagem('Erro interno do servidor. Tente novamente mais tarde.', 'danger');
-          throw new Error(`Erro ${response.status}: ${errorText}`);
-      }
-      else {
-          return response.json();
-      }
       const contentType = response.headers.get("content-type");
       if (contentType && contentType.includes("application/json")) {
-          return await response.json();
+        return await response.json();
       } else {
-          return await response.text(); // caso não seja JSON
+        return await response.text();
       }
+    } catch (error) {
+      console.error("[ApiClient] Erro:", error.message);
+      throw error;
+    }
   }
-
+  
   get(endpoint) {
     return this.request(endpoint, { method: 'GET' });
   }
