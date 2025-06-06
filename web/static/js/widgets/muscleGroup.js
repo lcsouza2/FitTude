010405cb3e4ff990588450272f281bd986c4f3bd
Luiz <lcsouzagarcia@gmail.com>
2025-06-06 20:46:44 -0300

    feat: code to display muscle groups on HTML

---
+++ web/static/js/widgets/muscleGroup.js
@@ -0,0 +1,8 @@
+import {MuscleGroup, MuscleGroupWidget, getAllMuscleGroups} from './core/mapping/muscleGroupMapping.js';
+
+let currentMuscleGroups = await getAllMuscleGroups()
+
+currentMuscleGroups.array.forEach(element => {
+   const muscleGroupWidget = new MuscleGroupWidget(element.id, element.name);
+   document.body.appendChild(muscleGroupWidget.render());
+});
