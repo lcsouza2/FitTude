4d0c82295d98696179d6b6731549d3f937bb97d3
Luiz <lcsouzagarcia@gmail.com>
2025-06-06 21:47:22 -0300

    feat: new class TokenManager that stores the token

--- web/static/js/core/auth.js
+++ web/static/js/core/auth.js
@@ -1,63 +1,95 @@
-import { exibirMensagem } from './utils.js'; //vou usar ainda
-export class ApiClient {
-  constructor(baseURL = '') {
-    this.baseURL = baseURL;
-  }
-  async request(endpoint, options = {}) {
-    try {
-      const finalOptions = {
-        credentials: 'include',
-        ...options
-      };
+import { exibirMensagem, BASE_URL } from "./utils.js"; //vou usar ainda
 
-      const response = await fetch(this.baseURL + endpoint, finalOptions);
-
-      if (!response.ok) {
-        const msg = await response.text();
-        throw new Error(`code ${response.status} :${msg}`);
-      }
-      let data;
-      const bodyType = response.headers.get('Content-Type');
-      if (bodyType && bodyType.includes('application/json')) {
-        data = await response.json() 
-      }
-      else {
-        data = await response.text() 
-      }
-      
-      return {
-        headers: response.headers,
-        body: data
-      };
-
-    } catch (error) {
-      console.error("[ApiClient] :", error.message);
-      throw new Error (error);
+class TokenManager {
+    constructor() {
+        this.sessionToken = null;
+        this.refreshPromise = null;
+        this.isRefreshing = false;
     }
-  }
 
-  get(endpoint) {
-    return this.request(endpoint, { method: 'GET' });
-  }
+    setSessionToken(token) {
+        this.sessionToken = token;
+    }
 
-  post(endpoint, body) {
-    return this.request(endpoint, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body),
-    });
-  }
+    getSessionToken() {
+        return this.sessionToken;
+    }
 
-  put(endpoint, body) {
-    return this.request(endpoint, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body),
-    });
-  }
+    clearTokens() {
+        this.sessionToken = null;
+        this.refreshPromise = null;
+        this.isRefreshing = false
+    }
 
-  delete(endpoint) {
-    return this.request(endpoint, { method: 'DELETE' });
-  }
+    async refreshSessionToken() {
+        if (this.isRefreshing) {
+            return this.refreshPromise;
+        }
 
+        this.isRefreshing = true;
+        
+        this.refreshPromise = fetch(BASE_URL + "/api/")
+    }
+}
+
+export class ApiClient {
+constructor(protected ) {
+    this.protected = protected
+    this.baseURL = BASE_URL;
+}
+
+async request(endpoint, options = {}) {
+    try {
+
+
+        const finalOptions = {
+            credentials: "include",
+            Authorization: this.protected ? `Bearer ${localStorage.getItem("token")}` : undefined,
+            ...options,
+        };
+
+        const response = await fetch(this.baseURL + endpoint, finalOptions);
+
+            let data;
+        const bodyType = response.headers.get("Content-Type");
+
+        if (bodyType && bodyType.includes("application/json")) {
+            data = await response.json();
+        } else {
+            data = await response.text();
+        }
+
+        return {
+            headers: response.headers,
+            body: data,
+        };
+    } catch (error) {
+        console.error("[ApiClient] :", error.message);
+        throw new Error(error);
+    }
+}
+
+    get(endpoint) {
+        return this.request(endpoint, { method: "GET"});
+    }
+
+    post(endpoint, body) {
+        return this.request(endpoint, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+        });
+    }
+
+    put(endpoint, body) {
+        return this.request(endpoint, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+        });
+    }
+
+    delete(endpoint) {
+        return this.request(endpoint, { method: "DELETE" });
+    }
 }
