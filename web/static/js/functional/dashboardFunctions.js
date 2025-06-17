de2d5cc0495eb76f664ec8c9a5c97a5186ef2306
JoaoNike <jaogabrie00@gmail.com>
2025-06-17 01:56:29 -0300

    fix: corrige a atribuição do token e melhora a organização do código no dashboard

--- web/static/js/functional/dashboardFunctions.js
+++ web/static/js/functional/dashboardFunctions.js
@@ -1,13 +1,8 @@
 import { authApiClient, tokenManager } from '../core/auth.js';
+import { populateUsername } from '../visual/dashboardVisuals.js';
 
-tokenManager.getSessionToken()? undefined : tokenManager.refreshSessionToken();
-
-
-
-
-
-
-
+const token = tokenManager.getSessionToken()? undefined : tokenManager.refreshSessionToken();
+populateUsername("Joao")
 
 const logoffButton = document.getElementById('logoff');
 logoffButton.addEventListener('click', function() {
