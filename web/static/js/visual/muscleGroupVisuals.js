ddcf94877eec48473a3be5dd20324155ffe58798
JoaoNike <jaogabrie00@gmail.com>
2025-06-18 01:59:49 -0300

    feat: adiciona evento de clique para o botão de novo grupamento

--- web/static/js/visual/muscleGroupVisuals.js
+++ web/static/js/visual/muscleGroupVisuals.js
@@ -1,7 +1,13 @@
+import { Colors } from "chart.js";
+import { getAllMuscleGroups, MuscleGroup } from "../core/mapping/muscleGroupMapping";
+
 export function showFormModal() {
     const modal = new bootstrap.Modal(document.getElementById('novoGrupamentoModal'));
     modal.show();
 }
+document.getElementById('newgrup').addEventListener('click', function(){
+    
+})
 
 document.querySelector('.button-style').addEventListener('click', function() {
     
